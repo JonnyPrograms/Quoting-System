217ed9dd32dc4d118505c81e63b6fbb54de37230
Jonathan Castro <133724360+JonnyPrograms@users.noreply.github.com>
2024-12-18 22:39:20 -0600

    Add files via upload

---
+++ QuoteSystem.API/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using QuotesSystem.Infrastructure.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Demo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly ITestService _testService;
+
+        public DemoController(ITestService demoService)
+        {
+            _testService = demoService;
+        }
+
+        [HttpGet(Name = "GetName")]
+        public IEnumerable<string> GetName()
+        {
+            List<string> names = _testService.GetName();
+
+            return names;
+        }
+
+    }
+}
